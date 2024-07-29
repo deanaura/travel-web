4855de108e2375396e7701d6c88f6ebdfe927f85
Dean Oki <75497106+Deanoki@users.noreply.github.com>
2024-07-29 13:14:56 +0700

    navbar dulu

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("menu-toggle").addEventListener("click", function () {
+  document.querySelector(".menu-list").classList.toggle("active");
+  document.querySelector(".btn-list").classList.toggle("active");
+});
