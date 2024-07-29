bea7a168fb97e54d4a1bcb721c4683afb7f2828f
Deanoki <deanoki2808@gmail.com>
2024-07-29 13:32:43 +0700

    version 1

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("menu-toggle").addEventListener("click", function () {
-  document.querySelector(".menu-list").classList.toggle("active");
-  document.querySelector(".btn-list").classList.toggle("active");
-});
